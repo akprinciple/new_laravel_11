f8f5a650f70b97c2479c4271ae50d74ee8861b70
akprinciple <akeemolayiwola9@gmail.com>
2024-04-22 00:22:27 +0100

    Real Commit

---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use function Pest\Laravel\get;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/home', function(){
+    return view('home');
+});
+Route::get('/test', function(){
+    
+    return [['name'=>'Akeem', 'age'=>12], ['name'=>'Dav', 'text'=> 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio quia iusto ad deleniti enim distinctio mollitia ipsa obcaecati. A sed sapiente omnis vitae amet non ut officiis quisquam eum blanditiis!']];
+});
