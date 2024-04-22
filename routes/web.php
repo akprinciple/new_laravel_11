ce539036d85a68491b0ee33ba0d9e69f81864c42
akprinciple <akeemolayiwola9@gmail.com>
2024-04-22 02:10:41 +0100

    New commit

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,12 @@ Route::get('/', function () {
 Route::get('/home', function(){
     return view('home');
 });
+Route::get('/about', function(){
+    return view('about');
+});
+Route::get('/contact', function(){
+    return view('contact');
+});
 Route::get('/test', function(){
     
     return [['name'=>'Akeem', 'age'=>12], ['name'=>'Dav', 'text'=> 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio quia iusto ad deleniti enim distinctio mollitia ipsa obcaecati. A sed sapiente omnis vitae amet non ut officiis quisquam eum blanditiis!']];
